ce0b52e98e41af81c6f4bc2d9a31595a0ed9957b
Denis <denokim55@gmail.com>
2022-06-27 01:56:20 +0300

    Added some transitions

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,70 @@
 <template>
   <div :class="['bg-[#fff] h-screen grid sm:justify-center sm:items-center', { dark: isDark }]">
     <!-- Calculator component -->
-    <div class="bg-[#F1F3F6] dark:bg-[#141518] px-5 py-8 w-full sm:w-[397px]">
-      <div class="flex justify-end">
-        <!-- Light mode/Dark mode switch -->
-        <button @click="handleThemeChange" class="p-0.5 mb-8 rounded-full bg-[#f1f3f6] dark:shadow-neoDark shadow-neo dark:bg-[#141518] focus:ring-0">
-          <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-[#1565C0] dark:text-[#F1F3F6]" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </button>
-      </div>
-      <!-- Input -->
-      <div class="p-3 bg-[#f1f3f6] dark:bg-[#141518] !text-right shadow-neoInput dark:shadow-neoDarkInput mb-16 rounded-[15px]">
-        <div class="h-[25px] mb-3">
-          <p class="px-8 text-xl text-right dark:text-white font-display">{{ previousOperand + " " + operand }}</p>
+    <FadeInOut entry="center" exit="center" :duration="300" appear>
+      <div class="bg-[#F1F3F6] dark:bg-[#141518] px-5 py-8 w-full sm:w-[397px]">
+        <div class="flex justify-end">
+          <!-- Light mode/Dark mode switch -->
+          <button @click="handleThemeChange" class="p-0.5 mb-8 rounded-full bg-[#f1f3f6] dark:shadow-neoDark shadow-neo dark:bg-[#141518] focus:ring-0">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-[#1565C0] dark:text-[#F1F3F6]" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+            </svg>
+          </button>
         </div>
-        <input placeholder="888888888888888888" v-model="currentOperand" type="text" name="input" id="input" class="w-full px-8 py-7 bg-transparent dark:bg-[#202226] border-gray-300 dark:border-gray-100 rounded-md dark:focus:border-transparent focus:ring-indigo-[#1565C0] focus:border-[#1565C0] sm:text-2xl dark:text-white text-right font-display" />
+        <FadeInOut entry="center" exit="center" :duration="800" appear>
+          <!-- Input -->
+          <div class="p-3 bg-[#f1f3f6] dark:bg-[#141518] !text-right shadow-neoInput dark:shadow-neoDarkInput mb-16 rounded-[15px]">
+            <div class="h-[25px] mb-3">
+              <p class="px-8 text-xl text-right dark:text-white font-display">{{ previousOperand + " " + operand }}</p>
+            </div>
+            <input placeholder="888888888888888888" v-model="currentOperand" type="text" name="input" id="input" class="w-full px-8 py-7 bg-transparent dark:bg-[#202226] border-gray-300 dark:border-gray-100 rounded-md dark:focus:border-transparent focus:ring-indigo-[#1565C0] focus:border-[#1565C0] sm:text-2xl dark:text-white text-right font-display" />
+          </div>
+        </FadeInOut>
+
+        <!-- Keyboard -->
+        <FadeInOut entry="center" exit="center" :duration="1000" appear>
+          <div class="grid grid-cols-4 grid-rows-4 gap-3">
+            <button class="button" data-all-clear>AC</button>
+            <button class="button" data-operation>$</button>
+            <button class="button" data-operation>%</button>
+            <button class="button border-[#1565C0] text-[#1565C0]" data-operation>/</button>
+            <button class="button" data-number>7</button>
+            <button class="button" data-number>8</button>
+            <button class="button" data-number>9</button>
+            <button class="button border-[#1565C0] text-[#1565C0]" data-operation>x</button>
+            <button class="button" data-number>4</button>
+            <button class="button" data-number>5</button>
+            <button class="button" data-number>6</button>
+            <button class="button border-[#1565C0] text-[#1565C0]" data-operation>-</button>
+            <button class="button" data-number>3</button>
+            <button class="button" data-number>2</button>
+            <button class="button" data-number>1</button>
+            <button class="button border-[#1565C0] text-[#1565C0]" data-operation>+</button>
+            <button class="col-span-2 button" data-number>0</button>
+            <button class="button" data-number>.</button>
+            <button class="button bg-[#1565C0] text-white border-[#F1F3F6] dark:!border-gray-900 dark:bg-[#1565C0]" data-equals>=</button>
+          </div>
+        </FadeInOut>
+
+        <!--  -->
       </div>
-      <!-- Keyboard -->
-      <div class="grid grid-cols-4 grid-rows-4 gap-3">
-        <button class="button" data-all-clear>AC</button>
-        <button class="button" data-operation>$</button>
-        <button class="button" data-operation>%</button>
-        <button class="button border-[#1565C0] text-[#1565C0]" data-operation>/</button>
-        <button class="button" data-number>7</button>
-        <button class="button" data-number>8</button>
-        <button class="button" data-number>9</button>
-        <button class="button border-[#1565C0] text-[#1565C0]" data-operation>x</button>
-        <button class="button" data-number>4</button>
-        <button class="button" data-number>5</button>
-        <button class="button" data-number>6</button>
-        <button class="button border-[#1565C0] text-[#1565C0]" data-operation>-</button>
-        <button class="button" data-number>3</button>
-        <button class="button" data-number>2</button>
-        <button class="button" data-number>1</button>
-        <button class="button border-[#1565C0] text-[#1565C0]" data-operation>+</button>
-        <button class="col-span-2 button" data-number>0</button>
-        <button class="button" data-number>.</button>
-        <button class="button bg-[#1565C0] text-white border-[#F1F3F6] dark:!border-gray-900 dark:bg-[#1565C0]" data-equals>=</button>
+    </FadeInOut>
+
+    <!--  -->
+    <SlideInOut entry="left" exit="left" :duration="800" appear>
+      <!-- Gif  -->
+      <div v-show="showGif" class="absolute hidden sm:block left-12 bottom-16">
+        <iframe src="https://giphy.com/embed/anuMFPLssX6SI" width="320" height="270" frameBorder="0" class="hidden xl:block giphy-embed" allowFullScreen></iframe>
       </div>
+
       <!--  -->
-    </div>
-    <!--  -->
-    <!-- Gifs  -->
-    <div v-show="showGif" class="absolute hidden sm:block left-12 bottom-16">
-      <iframe src="https://giphy.com/embed/anuMFPLssX6SI" width="320" height="270" frameBorder="0" class="hidden xl:block giphy-embed" allowFullScreen></iframe>
-    </div>
-    <!--  -->
+    </SlideInOut>
   </div>
 </template>
 
 <script>
+import { FadeInOut, SlideInOut } from "vue3-transitions";
+
 export default {
   name: "App",
 
@@ -62,10 +76,14 @@ export default {
     showGif: false,
   }),
 
+  components: { FadeInOut, SlideInOut },
+
   methods: {
     // Handle theme change
     handleThemeChange() {
       this.isDark = !this.isDark;
+      // Set the theme preference in localStorage
+      localStorage.setItem("isDark", this.isDark);
     },
 
     // This function handles the arithmetic operations.
@@ -137,6 +155,10 @@ export default {
   },
 
   mounted() {
+    // Check if the user has set a theme in localStorage
+    if (localStorage.getItem("isDark") === "true") {
+      this.isDark = true;
+    }
     // Listen to the keydown event of the input buttons
     const numberButtons = document.querySelectorAll("[data-number]");
     numberButtons.forEach((button) => {
